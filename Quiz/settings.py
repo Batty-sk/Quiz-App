33d6d9b2c67a3127583880cdb4d528dab7f1c98f
Batty-sk <saorav.skumar@gmail.com>
2023-12-14 21:09:21 +0530

    mission successfully

--- Quiz/settings.py
+++ Quiz/settings.py
@@ -130,7 +130,7 @@ STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
-
+#deployed successfully 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
